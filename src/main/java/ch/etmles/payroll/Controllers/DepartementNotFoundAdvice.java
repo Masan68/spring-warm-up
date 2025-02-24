450ac8bf42334a6efe4473b8639e568b6afe3704
Sam <sam.dreneau@eduvaud.ch>
2025-02-24 12:22:38 +0100

    DepartementNotFoundAdvice

---
+++ src/main/java/ch/etmles/payroll/Controllers/DepartementNotFoundAdvice.java
@@ -0,0 +1,27 @@
+package ch.etmles.payroll.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class DepartementNotFoundAdvice {
+
+
+    @ResponseBody
+    @ExceptionHandler(DepartementNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String departementNotFoundHandler(DepartementNotFoundException ex){
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(DepartementNotFoundDeleted.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    String departementNotFoundHandler(DepartementNotFoundDeleted ex){
+        return ex.getMessage();
+    }
+
+}
