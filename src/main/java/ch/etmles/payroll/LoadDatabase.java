f77ae5c744ae3f905dd2be83d219acb846e62fa3
Sam <sam.dreneau@eduvaud.ch>
2025-03-03 12:00:06 +0100

    build : config mysql

--- src/main/java/ch/etmles/payroll/LoadDatabase.java
+++ src/main/java/ch/etmles/payroll/LoadDatabase.java
@@ -17,21 +17,21 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(DepartementRepository departementRepository, EmployeeRepository employeeRepository) {
         return args->{
-            DepartementEntity itDepartement = new DepartementEntity("IT");
+            /*DepartementEntity itDepartement = new DepartementEntity("IT");
             log.info("Preloading " + departementRepository.save(itDepartement));
-            /*********************/
+            /********************
             //Departement itDepartement1 = new Departement("IT");
             //log.info("Preloading " + departementRepository.save(itDepartement1)); // Le nom doit être unique donc ici aussi un problème
-            /*********************/
 
             DepartementEntity secretariat = new DepartementEntity("secretariat");
             log.info("Preloading " + departementRepository.save(secretariat));
 
             log.info("Preloading " + employeeRepository.save(new EmployeeEntity("Bilbo Baggins", "burglar", "biblo@test")));
-            /*********************/
+            /********************
             log.info("Preloading " + employeeRepository.save(new EmployeeEntity("Frodo Baggins", "thief", "Frodo@test")));
             //log.info("Preloading " + employeeRepository.save(new Employee("Jjo Baggins", "thief", "Frodo@test")));// Email doit être unique donc ici aussi un problème
-            /*********************/
+            /********************
+            */
 
         };
     }
